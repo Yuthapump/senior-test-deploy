a04d6ea0e155bdca6f0ee8672fca5d965668a953
Yuthapum <benzzappn@gmail.com>
2024-10-01 13:55:41 +0700

    fix add upload fn

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,7 +1,7 @@
 // profileRoute.js
 const express = require("express");
-//const multer = require("multer");
-const { upload } = require("../server");
+const multer = require("multer");
+//const { upload } = require("../server");
 
 const router = express.Router();
 
@@ -9,6 +9,37 @@ const {
   updateProfilePic,
   getProfilePic,
 } = require("../controllers/profileController");
+
+// ตั้งค่า multer สำหรับจัดการ multipart/form-data (การอัพโหลดไฟล์)
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // กำหนดโฟลเดอร์สำหรับเก็บไฟล์ที่อัพโหลด
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    ); // ตั้งชื่อไฟล์ใหม่พร้อมนามสกุลเดิม
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 20 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    const filetypes = /jpeg|jpg|png/; // รองรับไฟล์ JPEG, JPG และ PNG
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+    cb(new Error("กรุณาอัพโหลดไฟล์รูปภาพที่เป็นนามสกุล jpeg, jpg, หรือ png"));
+  },
+});
 
 // Route สำหรับการอัปเดตภาพโปรไฟล์
 router.put(
