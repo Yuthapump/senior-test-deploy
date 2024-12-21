a0ba613e779758ce7b5ac0e167dd436f027a441d
Yuthapum <benzzappn@gmail.com>
2024-12-21 18:13:37 +0700

    fix assessment function by child_id and aspect

--- routes/assessmentRoutes.js
+++ routes/assessmentRoutes.js
@@ -2,6 +2,10 @@
 const express = require("express");
 const router = express.Router();
 const assessmentController = require("../controllers/assessmentController");
+const {
+  getAssessmentsByAspect,
+} = require("../controllers/assessmentController");
+// const { protect, restrictTo } = require("../controllers/authController");
 
 // Route to fetch assessments by child ID
 router.get(
@@ -9,16 +13,13 @@ router.get(
   assessmentController.getAssessmentsByChild
 );
 
-// Route to update an assessment result and get the next assessment
+// Route to update an assessment result and get next assessment
 router.post(
   "/assessments-update-status/:child_id/:aspect",
   assessmentController.updateAssessmentStatus
 );
 
 // Route to fetch assessment details by child_id and aspect
-router.get(
-  "/assessments-get-details/:child_id/:aspect",
-  assessmentController.getAssessmentsByAspect
-);
+router.get("/assessments-get-details", getAssessmentsByAspect);
 
 module.exports = router;
