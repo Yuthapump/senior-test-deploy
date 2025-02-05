d26027921251d0ad447b8f807b3b1778a20285d0
Yuthapum <benzzappn@gmail.com>
2025-02-05 12:11:14 +0700

    fix resetLink

--- controllers/authController.js
+++ controllers/authController.js
@@ -199,7 +199,8 @@ const forgetPassword = async (req, res) => {
       },
     });
 
-    const resetLink = `DekDekApp://reset-password?token=${resetToken}`;
+    const resetLink = Linking.createURL(`/reset-password?token=${resetToken}`); // For Test
+    // const resetLink = `DekDekApp://reset-password?token=${resetToken}`; // สำหรับ Mobile App
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to: email,
