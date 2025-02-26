75ae27b3a9acfeaa480aad1e2443c8d4c11ffe84
Yuthapum <benzzappn@gmail.com>
2025-02-26 17:23:03 +0700

    test 404 to 200

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -118,7 +118,7 @@ const getRoomData = async (req, res) => {
     );
 
     if (rooms.length === 0) {
-      return res.status(404).json({
+      return res.status(200).json({
         message: "No rooms found for this supervisor",
         rooms: [],
       });
